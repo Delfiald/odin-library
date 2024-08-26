efb01aad3e288a22200aa66c366b1a891cb8e37d
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-26 23:13:45 +0700

    Adding Header
    adding header with nav
    -hero text
    -menu with button and menu item inside it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const searchBtn = document.querySelector('.search');
+const menuBtn = document.querySelector('.menu');
+
+document.addEventListener('click', (e) => {
+  if(e.target.closest('.search i')){
+    searchBtn.classList.toggle('open');
+  }else if(e.target.closest('.menu .menu-button')){
+    menuBtn.classList.toggle('open');
+  }
+})
