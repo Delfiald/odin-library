a7e2d4c4db60638cefa4f89a3a037f397150c4c0
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-27 22:32:14 +0700

    Adding Content and More Styling
    adding main content with 4 sections
    -main
    -completed
    -not read yet
    -not finished
    
    also adding add book using dialog

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-const searchBtn = document.querySelector('.search');
 const menuBtn = document.querySelector('.menu');
+const searchBox = document.querySelector('.search-box');
+const addBook = document.querySelector('.add-book');
+
+const searchBoxHandler = (e) => {
+  if(e.target.closest('i:first-child')){
+    searchBox.classList.remove('show');
+  }else if(e.target.closest('i:last-child')){
+    searchBox.querySelector('input').value = '';
+  }
+}
 
 document.addEventListener('click', (e) => {
   if(e.target.closest('.search i')){
-    searchBtn.classList.toggle('open');
+    searchBox.classList.toggle('show');
   }else if(e.target.closest('.menu .menu-button')){
     menuBtn.classList.toggle('open');
+  }else if(e.target.closest('.search-box .close')){
+    searchBoxHandler(e);
+  }else if(e.target.closest('.add-book-btn')) {
+    console.log(e.target)
+    addBook.showModal();
+  }else if(e.target.closest('.add-book .close')){
+    console.log(e.target)
+    addBook.close();
   }
 })
